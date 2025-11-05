cf961edd8cd79fc4c9a5b18b43dcb1498bc5fc0a
Saronzeleke <Cassiopiasaron@gmail.com>
2025-11-05 21:25:36 +0300

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,19 +2,25 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import authRoutes from './routes/authRoutes.js';
 
-dotenv.config({path: './env'});
+dotenv.config({ path: './env' });
 const app = express();
 
+// MIDDLEWARE
+app.use(helmet()); // Adds security headers
+app.use(morgan('dev')); // Logs HTTP requests
 app.use(cors());
-app.use(express.json({ strict: false}));
+app.use(express.json({ strict: false }));
 app.use(cookieParser());
 
-//ROUTES
+// ROUTES
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT;
+// START SERVER
+const PORT = process.env.PORT || 5000; // default fallback
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
