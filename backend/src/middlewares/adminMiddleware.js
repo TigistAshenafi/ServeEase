890653dd3f0bf41db0c593e966ec6b5c5f3e8f3c
TigistAshenafi <tigistashenafi42@gmail.com>
2025-12-08 17:15:34 +0300

    lastUpdate

--- backend/src/middlewares/adminMiddleware.js
+++
@@ -1,6 +0,0 @@
-export default function adminMiddleware(req, res, next) {
-  if (!req.user || req.user.role !== 'admin') {
-    return res.status(403).json({ message: "Forbidden: Admins only" });
-  }
-  next();
-}
